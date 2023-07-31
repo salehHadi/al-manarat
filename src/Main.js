df18ec2bfe3647afc19c93c7c97eff41dcb61c6e
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2023-07-31 14:57:55 +0300

    about us page 1st v1

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./Pages/Home";
 import OurProjects from "./Pages/OurProjects";
+import AboutUs from "./Pages/AboutUs";
 
 const Main = () => {
   return (
     <div>
       <Home />
       <OurProjects />
+      <AboutUs />
     </div>
   );
 };
---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,20 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+import "../Styles/pageStyles.css";
+
+const AboutUs = () => {
+  return (
+    <div className="aboutUs-container">
+      <Container>
+        <Typography variant="h5" component="h2" className="aboutUs-text">
+          عن المنارات الشامخة
+        </Typography>
+        <div>
+          <img className="aboutUs-img" src="https://i.ibb.co/C7d9HPR/1.png" />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default AboutUs;
