e9ed559846f799de78931fbbce70f0bea8e74137
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2023-08-03 16:39:31 +0300

    adject the form to the backend code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardContent, Grid, TextField, Typography } from "@mui/material";
 import "../Styles/componentStyle.css";
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
   const style = {
     borderTopLeftRadius: "25px",
     borderBottomRightRadius: "25px",
@@ -11,64 +14,78 @@ const Form = () => {
   };
 
   return (
-    <CardContent className="form">
-      <Typography variant="h6" component="h2" fontWeight={"bold"}>
-        يسعدنا تواصلك
-      </Typography>
-      <Grid>
-        <Grid className="box" item marginBottom={1}>
-          <TextField
-            label="الاسم"
-            placeholder="الاسم الثنائي"
-            variant="outlined"
-            className="box"
-            required
-            inputProps={{
-              style: { textAlign: "right" },
-            }}
-            InputProps={{
-              style: style,
-            }}
-          />
+    <form method="post" action="http://localhost:4000/api/v1/form">
+      <CardContent className="form">
+        <Typography variant="h6" component="h2" fontWeight={"bold"}>
+          يسعدنا تواصلك
+        </Typography>
+        <Grid>
+          <Grid className="box" item marginBottom={1}>
+            <TextField
+              name="name"
+              type="String"
+              value={name}
+              onInput={(e) => setName(e.target.value)}
+              label="الاسم"
+              placeholder="الاسم الثنائي"
+              variant="outlined"
+              className="box"
+              required
+              inputProps={{
+                style: { textAlign: "right" },
+              }}
+              InputProps={{
+                style: style,
+              }}
+            />
+          </Grid>
+          <Grid className="box" item marginBottom={1}>
+            <TextField
+              label="الجوال"
+              type="number"
+              name="phoneNumber"
+              value={phoneNumber}
+              onInput={(e) => setPhoneNumber(e.target.value)}
+              variant="outlined"
+              className="box"
+              placeholder="05XXXXXXXX"
+              required
+              inputProps={{
+                style: { textAlign: "right" },
+              }}
+              InputProps={{
+                style: style,
+              }}
+            />
+          </Grid>
+          <Grid item marginBottom={1}>
+            <TextField
+              type="String"
+              label="الطلب"
+              name="message"
+              value={message}
+              onInput={(e) => setMessage(e.target.value)}
+              variant="outlined"
+              className="box"
+              InputLabelProps={{ className: "box__cus" }}
+              multiline
+              rows={4}
+              inputProps={{
+                style: { textAlign: "right" },
+              }}
+              InputProps={{
+                style: style,
+              }}
+            />
+          </Grid>
+          <Grid xs={12} sm={4} item>
+            <button type="submit" className="form-btn">
+              أرسل
+            </button>
+          </Grid>
         </Grid>
-        <Grid className="box" item marginBottom={1}>
-          <TextField
-            label="الجوال"
-            type="number"
-            variant="outlined"
-            className="box"
-            placeholder="05XXXXXXXX"
-            required
-            inputProps={{
-              style: { textAlign: "right" },
-            }}
-            InputProps={{
-              style: style,
-            }}
-          />
-        </Grid>
-        <Grid item marginBottom={1}>
-          <TextField
-            type="message"
-            label="الطلب"
-            variant="outlined"
-            className="box"
-            InputLabelProps={{ className: "box__cus" }}
-            multiline
-            rows={4}
-            inputProps={{
-              style: { textAlign: "right" },
-            }}
-            InputProps={{
-              style: style,
-            }}
-          />
-        </Grid>
-        <Grid xs={12} sm={4} item>
-          <button className="form-btn">أرسل</button>
-        </Grid>
-      </Grid>
-    </CardContent>
+      </CardContent>
+    </form>
   );
 };
 
